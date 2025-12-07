d05376ee07eb999379f6027b2bcfc11e700ea871
chris <chris@zombie>
2025-12-07 18:54:49 +0100

    initialialise tvguide-ng workspace

--- html-v1/xsl/doTVGuide.cmd
+++
@@ -1,4 +0,0 @@
-TITLE Generating Chris TV Guide
-java -Xmx512M -Dlogback.configurationFile=".\logback.xml" -jar HTMLMaker.jar -xml "..\xml\mergexmltv.xmltv" -xsl "..\bin\tvguide.xsl" -xfrm "00favorites.htm" -out "..\tv" -fav "..\crit\favcrit.xml"
-
-java -Xmx512M -Dlogback.configurationFile=".\logback.xml" -jar HTMLMaker.jar -xml "..\xml\mergexmltv.xmltv" -xsl "..\bin\tvguide-ng.xsl" -xfrm "00favorites.htm" -out "..\tv-ng" -fav "..\crit\favcrit.xml"
